19d894922c5cc8c0fd6ed69fcc22693626fcff4e
Erik Giron <kreig.iron@gmail.com>
2018-08-29 18:51:47 -0600

    Rename and add usages

--- spotisync.py
+++ spotisync.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python3
+
 # more info on the M3U file format available here:
 # http://n4k3d.com/the-m3u-file-format/
 
 import sys
+
 
 class track():
     def __init__(self, length, title, path):
         self.length = length
         self.title = title
         self.path = path
+
 
 """
     song info lines are formatted like:
@@ -17,6 +21,7 @@ class track():
     file name - relative or absolute path of file
     ..\Minus The Bear - Planet of Ice\Minus The Bear_Planet of Ice_01_Burying Luck.mp3
 """
+
 
 def parsem3u(infile):
     try:
@@ -55,13 +60,29 @@ def parsem3u(infile):
 
     return playlist
 
+
 # for now, just pull the track info and print it onscreen
 # get the M3U file path from the first command line argument
+def print_usage():
+    print("Spotisync:")
+    print("Usage: spotysinc [path-to-m3u-playlist] [playlist-name-on-spotify]")
+    print()
+    print("path-to-m3u-playlist: Path of the m3u playlist to sync")
+    print("playlist-name-on-spotify: Playlist destination on spotify, will overwrite it if exists")
+
+
 def main():
+    if len(sys.argv) != 2:
+        print_usage()
+        exit(0)
+
     m3ufile=sys.argv[1]
-    playlist = parseM3U(m3ufile)
+    spotify_playlist_name = sys.argv[2]
+
+    playlist = parsem3u(m3ufile)
     for track in playlist:
-        print (track.title, track.length, track.path)
+        print(track.title, track.length, track.path)
+
 
 if __name__ == '__main__':
     main()
